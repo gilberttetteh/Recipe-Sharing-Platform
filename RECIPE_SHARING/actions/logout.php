ebc5253ea2f04148e64de70e094555ebcb335fb4
Gilbert Andrew Tetteh <144915635+gilberttetteh@users.noreply.github.com>
2024-12-30 22:36:15 +0000

    Delete RECIPE_SHARING directory

--- RECIPE_SHARING/actions/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['signout'] = "You have successfully signed out.";
-session_write_close();
-session_unset();
-session_destroy();
-header("Location: ../view/login.php");
-exit;
